435443cc83ad4944859cc839ff79ce3580fff9a1
tranloi15 <tranducloi784@gmail.com>
2026-08-13 08:34:29 +0700

    tinh nang phan trang

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus.js");
 const searchHelper = require("../../helpers/search.js");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
@@ -15,18 +16,37 @@ module.exports.index = async (req, res) => {
 
     const objectSearch = searchHelper(req.query);
 
-    console.log(objectSearch);
-
     if (objectSearch.regex) {
         find.title = objectSearch.regex;
     }
 
-    const products = await Product.find(find);
+    // Pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItems: 4
+    };
+
+    if (req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
+
+    // Sửa Product.count thành Product.countDocuments
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts / objectPagination.limitItems);
+    objectPagination.totalPage = totalPage;
+    // End Pagination
+
+    const products = await Product.find(find)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -38,3 +38,20 @@ if (formSearch) {
     });
 }
 // End Form Search
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if (buttonsPagination.length > 0) {
+    let url = new URL(window.location.href);
+
+    buttonsPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+
+            url.searchParams.set("page", page);
+
+            window.location.href = url.href;
+        });
+    });
+}
+// End Pagination
