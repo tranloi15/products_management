e4644d2d0fc5837efb7e9cc6d769df675918711b
tranloi15 <tranducloi784@gmail.com>
2026-08-19 15:57:32 +0700

    tao moi san pham (upload anh)

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
+const storageMulter = require("../../helpers/storageMulter");
+const upload = multer({ storage: storageMulter() });
 const controller = require("../../controller/admin/product.controller");
 
 // [GET] /admin/products 
@@ -16,5 +19,7 @@ router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
 
 router.post("/create", controller.createPost);
+
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 
 module.exports = router;
