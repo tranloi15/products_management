5a3c288f5696a77a6788a6de61021ce7e6786868
tranloi15 <tranducloi784@gmail.com>
2026-08-22 15:25:55 +0700

    trang chi tiet san pham client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 
+//[GET]/products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
@@ -18,3 +19,24 @@ module.exports.index = async (req, res) => {
         products: newProducts
     });
 }
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            slug: req.params.slug,
+            status: "active"
+        };
+
+        const product = await Product.findOne(find);
+
+        console.log(product);
+
+        res.render("client/pages/products/detail", {
+            pageTitle: product.title,
+            product: product
+        });
+    } catch (error) {
+        res.redirect(`/products`);
+    }
+};
