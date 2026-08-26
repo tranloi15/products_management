973e55ebbfbf43478af6e7f1ae48096e96a9dc11
tranloi15 <tranducloi784@gmail.com>
2026-08-26 22:31:14 +0700

    xay dung phan quan ly danh muc san pham (phan 1)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const systemConfig = require("../../config/system");
 
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
+const productCategoryRoutes = require("./product-category.route");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -12,4 +13,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
 
     app.use(PATH_ADMIN + "/products", productRoutes);
+
+    app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
 }
