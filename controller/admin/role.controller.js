b4dd30eddc1f2c653556210f1c928111a07a4fa0
tranloi15 <tranducloi784@gmail.com>
2026-09-01 17:27:21 +0700

    xay dung phan chinh sua nhom quyen

--- controller/admin/role.controller.js
+++ controller/admin/role.controller.js
@@ -35,3 +35,40 @@ module.exports.createPost = async (req, res) => {
         res.redirect(`${systemConfig.prefixAdmin}/roles/create`);
     }
 };
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false
+        };
+
+        const data = await Role.findOne(find);
+
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Sửa nhóm quyền",
+            data: data
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+};
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne(
+            { _id: id },
+            req.body
+        );
+
+        req.flash("success", "Cập nhật nhóm quyền thành công!");
+    } catch (error) {
+        req.flash("error", "Cập nhật nhóm quyền thất bại!");
+    }
+
+    res.redirect("back");
+};
