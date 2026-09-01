78384cb01f9b811e80d58176012a16ec55f1d896
tranloi15 <tranducloi784@gmail.com>
2026-09-01 17:15:51 +0700

    xay dung danh sach nhom quyen

---
+++ models/role.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permission: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deleteAt: Date
+}, {
+    timestamps: true
+});
+
+const role = mongoose.model("role", roleSchema, "roles");
+
+module.exports = role;
