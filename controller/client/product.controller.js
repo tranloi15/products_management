89645fe1db6fedfb1b9b34354a13b60e11ce4457
tranloi15 <tranducloi784@gmail.com>
2026-09-09 22:53:08 +0700

    trang danh sach san pham ben client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const ProductCategory = require("../../models/product-category.model");
 const productsHelper = require("../../helpers/products");
+const productsCategoryHelper = require("../../helpers/products-category");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -17,7 +18,7 @@ module.exports.index = async (req, res) => {
     });
 };
 
-// [GET] /products/:slug
+// [GET] /products/detail/:slug
 module.exports.detail = async (req, res) => {
     try {
         const find = {
@@ -40,6 +41,7 @@ module.exports.detail = async (req, res) => {
             });
             product.category = category;
         }
+
         product.priceNew = productsHelper.priceNewProduct
             ? productsHelper.priceNewProduct(product)
             : (
@@ -50,6 +52,39 @@ module.exports.detail = async (req, res) => {
         res.render("client/pages/products/detail", {
             pageTitle: product.title,
             product: product,
+        });
+    } catch (error) {
+        res.redirect("/products");
+    }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    try {
+        const category = await ProductCategory.findOne({
+            slug: req.params.slugCategory,
+            status: "active",
+            deleted: false,
+        });
+
+        if (!category) {
+            return res.redirect("/products");
+        }
+
+        const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+        const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+        const products = await Product.find({
+            product_category_id: { $in: [category.id, ...listSubCategoryId] },
+            status: "active",
+            deleted: false,
+        }).sort({ position: "desc" });
+
+        const newProducts = productsHelper.priceNewProducts(products);
+
+        res.render("client/pages/products/index", {
+            pageTitle: category.title,
+            products: newProducts,
         });
     } catch (error) {
         res.redirect("/products");
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,6 +5,6 @@ const controller = require("../../controller/client/product.controller");
 
 router.get("/", controller.index);
 
-router.get("/:slug", controller.detail);
+router.get("/:slugCategory", controller.category);
 
 module.exports = router;
