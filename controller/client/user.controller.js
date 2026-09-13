b18568789efc4b920913852aaefe7bce6d4ab46c
tranloi15 <tranducloi784@gmail.com>
2026-09-13 08:10:19 +0700

    dang nhap

--- controller/client/user.controller.js
+++ controller/client/user.controller.js
@@ -30,3 +30,39 @@ module.exports.registerPost = async (req, res) => {
 
     res.redirect("/");
 };
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập tài khoản",
+    });
+};
+
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        deleted: false
+    });
+
+    if (!user) {
+        req.flash("error", "Email không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if (md5(req.body.password) != user.password) {
+        req.flash("error", "Sai mật khẩu!");
+        res.redirect("back");
+        return;
+    }
+
+    if (user.status != "active") {
+        req.flash("error", "Tài khoản đang bị khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
+
+    res.redirect("/");
+};
